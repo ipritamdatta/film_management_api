7a4236af6531769f68efc35da290077260e48e1e
ipritamdatta <pritamewu@gmail.com>
2020-06-10 12:57:45 +0600

    Added pagination to the front page where all the films have being shown and created ajax get request in the index page

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -6,7 +6,8 @@
             
         </div>
         <div class="show_link">
-
+            <button class="prev-btn btn-light btn-outline-dark text-sm font-weight-bold">Prev Page</button>
+            <button class="next-btn btn-light btn-outline-dark text-sm font-weight-bold">Next Page</button>
         </div>
     </div>
 
@@ -20,42 +21,82 @@
         headers: {'X-CSRF-Token': '{{csrf_token()}}'}
     });
     //   get data
-    function getRecords(){
-        $.ajax({
-            type: 'GET', //THIS NEEDS TO BE GET
-            url: 'api/films',
-            dataType: 'json',
-            headers : {
-                "Accept" : "application/json; charset=utf-8", 
-                "Content-Type" : "application/json; charset=utf-8"
-            },
-            data:{ 
-                _token:'{{ csrf_token() }}'
-            },
-            success: function (response) {
-                console.log(response);
-                
-                // console.log(response.data[0].name);
-            var resultData = response.data;
-            var store = '';
-            $.each(resultData,function(index,row){
-                store += '<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-4">'
-                store += '<div class="card">'
-                store += '<img src="'+row.photo+'" class="card-img-top filmImageSize" alt="...">'
-                store += '<div class="card-body">'
-                store += '<p class="font-weight-bold">'+row.name+'</p>'
-                store += '<a href="#" class="btn btn-light btn-outline-dark text-sm font-weight-bold">view details</a>'
-                store += '</div>'
-                store += '</div>'
-                store += '</div>';
-            });
-            $(".show").html(store);
-                
-            },error:function(){ 
-                console.log(response);
+    $(function(){
+        var current_page = 1,
+            per_page = 3,
+            total = 0;
+
+        fetchData();
+
+        // prev btn
+        $(".prev-btn").on('click',function(){
+            if(current_page > 1){
+                current_page--;
+                fetchData();
             }
+            console.log("Prev page: "+current_page);
+            
         });
-    }
-    getRecords();
+        // next btn
+        $(".next-btn").on('click',function(){
+            if(current_page * per_page < total){
+                current_page++;
+                fetchData();
+            }
+            console.log("Next page: "+current_page);
+        });
+
+        function fetchData(){
+            // ajax
+            $.ajax({
+                type: 'GET', //THIS NEEDS TO BE GET
+                url: '/api/films?page='+current_page,
+                // dataType: 'json',
+                data:{
+                    current_page: current_page,
+                    per_page: per_page
+                },
+                headers : {
+                    "Accept" : "application/json; charset=utf-8", 
+                    "Content-Type" : "application/json; charset=utf-8"
+                },
+                success: function (data) {
+                console.log(data);
+                    
+                    if(data){
+                        total = data.meta.total; 
+
+                        var dataArr = data.data;
+                        var store = '';
+                        for(var i=0; i<dataArr.length;i++){
+                            // console.log(dataArr[i].href.link.split('/'));
+                            var pathArray = dataArr[i].href.link;
+                            var id = pathArray.substring(pathArray.lastIndexOf('/') + 1);
+
+
+                            store += '<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-4">'
+                            store += '<div class="card">'
+                            store += '<img src="'+dataArr[i].photo+'" class="card-img-top filmImageSize" alt="...">'
+                            store += '<div class="card-body">'
+                            store += '<p class="font-weight-bold">'+dataArr[i].name+'</p>'
+                            store += '<a href="/film/'+id+'" class="btn btn-light btn-outline-dark text-sm font-weight-bold">view details</a>'
+                            store += '</div>'
+                            store += '</div>'
+                            store += '</div>';
+                        }
+                         $(".show").html(store);
+                    }
+                
+                   
+
+                },error:function(){ 
+                    console.log(data);
+                }
+            });
+        }
+    });
+
+
+    
 </script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('films.index');
 });
+Route::get('/film/{id}','FilmFrontendController@show');
 
 Auth::routes();
