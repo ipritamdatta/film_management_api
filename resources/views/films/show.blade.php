afb39dbb15b6727c6c1968889d20cde65c1eabc6
ipritamdatta <pritamewu@gmail.com>
2020-06-10 14:21:30 +0600

    Displaying all the comments from api through ajax from individual film page

--- resources/views/films/show.blade.php
+++ resources/views/films/show.blade.php
@@ -5,6 +5,13 @@
         <div class="row show">
             
         </div>
+        {{-- display all the comment of this particular film --}}
+        <div class="bg-light p-5 m-5">
+            <div class="row">
+                <div class="mt-2 mb-4">COMMENTS</div>
+                <div class="showComment"></div>
+            </div>
+        </div>
     </div>
 @endsection
 
@@ -13,6 +20,8 @@
          $.ajaxSetup({
             headers: {'X-CSRF-Token': '{{csrf_token()}}'}
         });
+
+        // Showing individual data
         var getId = {{$id}};
         $.ajax({
             type: 'GET', //THIS NEEDS TO BE GET
@@ -40,6 +49,35 @@
                 store += '</div>'  
 
                 $(".show").html(store);
+            },error:function(){ 
+                console.log(data);
+            }
+        });
+
+        var getFilmId = {{$id}};
+        // show all comment associated with it
+        $.ajax({
+            type: 'GET', //THIS NEEDS TO BE GET
+            url: '/api/films/'+getFilmId+'/comments',
+            dataType: 'json',
+            headers : {
+                "Accept" : "application/json; charset=utf-8", 
+                "Content-Type" : "application/json; charset=utf-8"
+            },
+            success: function (data) {
+                console.log(data);
+
+                var getCommentData = data.data;
+                var storeComment ='';
+
+                for(var i=0;i< getCommentData.length;i++){
+                    storeComment += '<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">'
+                    storeComment +=     '<div>Name: <span class="font-weight-bold">'+getCommentData[i].name+'</span></div>'
+                    storeComment +=     '<div>Comment: '+getCommentData[i].comment+'</div>'
+                    storeComment +=     '<hr>'
+                    storeComment += '</div>'
+                }
+                $(".showComment").html(storeComment);
             },error:function(){ 
                 console.log(data);
             }
