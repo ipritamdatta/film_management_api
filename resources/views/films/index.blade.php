d9f423e9a2db216ce063320f0e0c7bd58bb602f6
ipritamdatta <pritamewu@gmail.com>
2020-06-10 13:53:30 +0600

    created show page and individual film's data have been shown using jquery ajax in show view

--- resources/views/films/index.blade.php
+++ resources/views/films/index.blade.php
@@ -73,16 +73,23 @@
                             var pathArray = dataArr[i].href.link;
                             var id = pathArray.substring(pathArray.lastIndexOf('/') + 1);
 
-
                             store += '<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12 mb-4">'
-                            store += '<div class="card">'
-                            store += '<img src="'+dataArr[i].photo+'" class="card-img-top filmImageSize" alt="...">'
-                            store += '<div class="card-body">'
-                            store += '<p class="font-weight-bold">'+dataArr[i].name+'</p>'
-                            store += '<a href="/film/'+id+'" class="btn btn-light btn-outline-dark text-sm font-weight-bold">view details</a>'
-                            store += '</div>'
-                            store += '</div>'
-                            store += '</div>';
+                            store +=    '<div class="card">'
+                            store +=        '<img src="'+dataArr[i].photo+'" class="card-img-top filmImageSize" alt="...">'
+                            store +=        '<div class="card-body">'
+                            store +=            '<p class="font-weight-bold">'+dataArr[i].name+'</p>'
+                            store +=            '<div class="row">'
+                            store +=                '<div class="col-lg-6 col-md-6 col-sm-6 col-sx-6">'
+                            store +=                    '<p class="font-weight-bold"><small>Release: '+dataArr[i].release+'</small></p>'
+                            store +=                '</div>'
+                            store +=                '<div class="col-lg-6 col-md-6 col-sm-6 col-sx-6">'
+                            store +=                    '<p class="font-weight-bold"><small>Rating: '+dataArr[i].rating+'</small></p>'
+                            store +=                '</div>'
+                            store +=            '</div>'
+                            store +=            '<a href="/film/'+id+'" class="btn btn-light btn-outline-dark text-sm font-weight-bold float-right">view details</a>'
+                            store +=        '</div>'
+                            store +=    '</div>'
+                            store +='</div>'
                         }
                          $(".show").html(store);
                     }
--- resources/views/films/show.blade.php
+++ resources/views/films/show.blade.php
@@ -1,1 +1,48 @@
-{{$id}}
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row show">
+            
+        </div>
+    </div>
+@endsection
+
+@section('ajaxdata')
+    <script>
+         $.ajaxSetup({
+            headers: {'X-CSRF-Token': '{{csrf_token()}}'}
+        });
+        var getId = {{$id}};
+        $.ajax({
+            type: 'GET', //THIS NEEDS TO BE GET
+            url: '/api/films/'+getId,
+            dataType: 'json',
+            headers : {
+                "Accept" : "application/json; charset=utf-8", 
+                "Content-Type" : "application/json; charset=utf-8"
+            },
+            success: function (data) {
+                var getData = data.data;
+                var store ='';
+                store += '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">'
+                store +=     '<img src="https://picsum.photos/seed/picsum/200/300" class="card-img-top filmImageSize" alt="...">'
+                store += '</div>'
+                store += '<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 mt-2 mb-4">'
+                store +=     '<p class="font-weight-bold">Film Name: '+getData.name+'</p>'
+                store +=     '<p class="font-weight-bold">Description: <small>'+getData.description+'</small></p>'
+                store +=     '<p class="font-weight-bold">Release: '+getData.release+'</p>'
+                store +=     '<p class="font-weight-bold">Date: '+getData.date+'</p>'
+                store +=     '<p class="font-weight-bold">Rating: '+getData.rating+'</p>'
+                store +=     '<p class="font-weight-bold">Ticket: '+getData.ticket+'</p>'
+                store +=     '<p class="font-weight-bold">Price: '+getData.price+'</p>'
+                store +=     '<p class="font-weight-bold">Country: '+getData.country+'</p>'
+                store += '</div>'  
+
+                $(".show").html(store);
+            },error:function(){ 
+                console.log(data);
+            }
+        });
+    </script>
+@endsection
